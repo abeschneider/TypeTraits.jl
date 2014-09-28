227eeb577f6f5a8b6ad34b205f2dfd2aa20bf02b
Abraham Schneider <abe.schneider@gmail.com>
2014-09-28 10:11:55 -0400

    initial code

--- src/TypeTraits.jl
+++ src/TypeTraits.jl
@@ -1,5 +1,47 @@
 module TypeTraits
 
-# package code goes here
+export @trait, @mixin
+
+# global registry of all abstract classes defined
+traits_declarations = Dict{Symbol, Array}()
+
+# @trait type X
+# ...
+# end
+macro trait(typedef)
+  # TODO: add checks that no inheritance occurs here
+
+  sym = typedef.args[2]
+  block = typedef.args[3]
+
+  traits_declarations[sym] = [Expr(:(::), var.args[1], var.args[2]) for var in block.args[2:2:end]]
+end
+
+# @mixin type Foo(X) <: Y end
+macro mixin(typedef)
+  decl = typedef.args[2]
+  block = typedef.args[3]
+
+  if decl.head === :call
+    sym = decl.args[1]
+    mixin = decl.args[2]
+    parent = None
+  elseif decl.head === :<:
+    traits = decl.args[1]
+    sym = traits.args[1]
+    mixin = traits.args[2]
+    parent = decl.args[2]
+  end
+
+  trait_declaration = traits_declarations[mixin]
+  child_declarations = [Expr(:(::), var.args[1], var.args[2]) for var in block.args[2:2:end]]
+  declarations = vcat(trait_declaration, child_declarations)
+
+  if parent !== None
+    Expr(:type, true, Expr(:<:, esc(sym), esc(parent)), Expr(:block, declarations...))
+  else
+    Expr(:type, true, esc(sym), Expr(:block, declarations...))
+  end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
 using TypeTraits
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+abstract Player
+
+@trait type Entity
+  id::Int64
+  x::Float64
+  y::Float64
+end
+
+@mixin type User(Entity) <: Player
+  name::String
+end
+
+function User(name::String)
+  User(0, 0, 0, name)
+end
+
+
+user = User("Foobar")
+println(user)
